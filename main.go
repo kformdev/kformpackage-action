6e5246613413a7bfc300769551a80f1a32851827
Wim Henderickx <wim.henderickx@gmail.com>
2023-11-20 17:52:22 +0100

    changed input handling/varibales

--- main.go
+++ main.go
@@ -6,9 +6,6 @@ import (
 )
 
 func main() {
-	pkgDir := os.Getenv("INPUT_PKG_DIR")
-	versionTag := os.Getenv("INPUT_VERSION_TAG")
-
-	fmt.Println("pkgDir", pkgDir)
-	fmt.Println("versionTag", versionTag)
+	fmt.Println("pkgDir", os.Getenv("INPUT_PKG_DIR"))
+	fmt.Println("versionTag", os.Getenv("INPUT_VERSION_TAG"))
 }
