fa8fd0696998cfe704562b708e727b167d9e22bd
Wim Henderickx <wim.henderickx@gmail.com>
2023-11-20 18:51:54 +0100

    update env

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	fmt.Println("args", os.Args)
 	fmt.Println("pkgDir", os.Getenv("INPUT_PKG_DIR"))
 	fmt.Println("versionTag", os.Getenv("INPUT_VERSION_TAG"))
+	fmt.Println("github token", os.Getenv("GITHUB_TOKEN"))
 }
