105bab6e5ae7e0edf70ba4c165e2ed7cdbc768e0
Wim Henderickx <wim.henderickx@gmail.com>
2023-11-21 07:08:57 +0100

    added refname

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ func main() {
 	fmt.Println("args", os.Args)
 	fmt.Println("pkgDir", os.Getenv("INPUT_PKG_DIR"))
 	fmt.Println("versionTag", os.Getenv("INPUT_VERSION_TAG"))
+
+	fmt.Println("reposiotry", os.Getenv("GITHUB_REPOSITORY"))
+	fmt.Println("refName", os.Getenv("GITHUB_REF_NAME"))
 	fmt.Println("github token", os.Getenv("GITHUB_TOKEN"))
 }
